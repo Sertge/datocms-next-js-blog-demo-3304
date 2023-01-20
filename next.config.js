f2cefdeb60a6d49bbe124c4386f3a14ab96115db
Sertge <sergiojaimito@gmail.com>
2023-01-19 19:41:10 -0500

    update configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-require('dotenv').config()
-
+/** @type {import('next').NextConfig} */
 module.exports = {
-  env: {
-    NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN:
-      process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
+  images: {
+    domains: ['www.datocms-assets.com'],
   },
 }
