9a20d1b22036d0a72dd22c403eeca59feb65e62f
Sertge <sergiojaimito@gmail.com>
2023-01-19 19:57:00 -0500

    update components to react 18

--- components/cover-image.js
+++ components/cover-image.js
@@ -15,10 +15,10 @@ export default function CoverImage({ title, responsiveImage, slug }) {
     />
   )
   return (
-    <div className="-mx-5 sm:mx-0">
+    <div className="sm:mx-0">
       {slug ? (
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a aria-label={title}>{image}</a>
+        <Link href={`/posts/${slug}`} aria-label={title}>
+          {image}
         </Link>
       ) : (
         image
