489f761ea4026223d3a901092291d607d813f51e
Sertge <sergiojaimito@gmail.com>
2023-01-30 09:32:40 -0500

    create sidebar component

---
+++ components/left-menu.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+
+type Post = {
+  title: string
+  coverImage: string
+  date: Date
+  author: string
+  slug: string
+}
+
+export default function LeftMenu ({allPosts}: {allPosts: Post[]}) {
+  allPosts.splice(3)
+  const router = useRouter()
+  console.log(router.pathname)
+  return (
+  <aside className={"width-extended bg-main-green font-lato text-brown justify-center text-xl px-2 top-12 absolute h-full z-20"}>
+    <div>
+      <div>Publicaciones Recientes</div>
+      <div className="px-4">
+        {allPosts.map((post) => {
+          return (
+            <div className="hover:bg-hover-green hover:text-lbrown rounded-md ">
+              <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+    <div>
+      <div>Otras utilidades</div>
+      <div className="px-4">
+        <div className="hover:bg-hover-green hover:text-lbrown rounded-md">Utilidad de costos</div>
+      </div>
+    </div>
+  </ aside>)
+}
